58ee7a349a17a69e1ba390786a3f56ccedace455
derek-j-pitman <70220747+derek-j-pitman@users.noreply.github.com>
2021-11-08 18:18:50 -0500

    Add some basic roster data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./styles.css";
 
-export default function App() {
+let roster_info = [
+  ["Derek", "Pitman", "green"],
+  ["Abdulwahab", "Murshed", "blue"],
+  ["Anton", "Schuster", "orange"],
+  ["Imani", "Taylor", "pink"]
+];
+
+function RosterRow(firstName, lastName, favoriteColor) {
   return (
-    <div className="App">
-      <div class="row">
-        <div>Derek</div>
-        <div>P</div>
-      </div>
+    <div className="row">
+      <span>{firstName}</span>
+      <span>{lastName}</span>
+      <span style={{ color: favoriteColor }}>{favoriteColor}</span>
     </div>
   );
+}
+export default function App() {
+  let rosterElements = [];
+  for (let person of roster_info) {
+    rosterElements.push(RosterRow(person[0], person[1], person[2]));
+  }
+  return <div className="App">{rosterElements}</div>;
 }
